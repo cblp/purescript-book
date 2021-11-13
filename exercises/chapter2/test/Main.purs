c5e4c16caec8337361751d37dabbff9090e78b46
Yuriy Syrovetskiy <git-orbi@cblp.su>
2021-11-13 12:06:57 +0300

    Finished chapter 2

--- exercises/chapter2/test/Main.purs
+++ exercises/chapter2/test/Main.purs
@@ -1,12 +1,14 @@
 module Test.Main where
 
 import Prelude
-import Test.MySolutions
+
 import Effect (Effect)
 import Euler (answer)
 import Test.Unit (suite, test)
 import Test.Unit.Assert as Assert
 import Test.Unit.Main (runTest)
+
+import Test.MySolutions
 
 main :: Effect Unit
 main = do
@@ -16,7 +18,6 @@ main = do
         Assert.equal 23 (answer 10)
       test "below 1000" do
         Assert.equal 233168 (answer 1000)
-    {-  Move this block comment starting point to enable more tests
     suite "diagonal" do
       test "3 4 5" do
         Assert.equal 5.0 (diagonal 3.0 4.0)
@@ -34,5 +35,3 @@ main = do
         Assert.equal 56 (leftoverCents 456)
       test "-789" do
         Assert.equal (-89) (leftoverCents (-789))
-
--}
